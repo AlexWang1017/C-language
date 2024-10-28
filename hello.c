8be9e94cd18b88e40d750fc2c7da88a4fb723146
AlexWang1017 <alexwang496@gmail.com>
2024-10-28 21:33:18 +0800

    nothing

--- hello.c
+++ hello.c
@@ -7,13 +7,15 @@ int big(int b,int c){
 
 int main(){
     int ans;
+    float a , b;
     ans = big(2,3);
     for(int i=0;i<=5;i++){
         printf("Hello world!\n");
     }
     printf("%d",ans);
     printf("I am Alex");
-    
+    a = 3;
+    printf("%d",a);
     return 0 ;
 
 }
